49b12f2b61e8712087650b1bb22a59a76e8439ed
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2022-06-07 00:09:28 +0200

    Initial commit.

---
+++ files.cmake
@@ -0,0 +1,16 @@
+set(H_FILES
+  qtlocalpeer.h
+  qtsingleapplication.h
+  qtsinglecoreapplication.h
+  qthandlenewappinstance.h
+)
+
+set(CPP_FILES
+  qthandlenewappinstance.h
+  qtlockedfile.h
+
+  qthandlenewappinstance.cpp
+  qtlocalpeer.cpp
+  qtsingleapplication.cpp
+  qtsinglecoreapplication.cpp
+)
